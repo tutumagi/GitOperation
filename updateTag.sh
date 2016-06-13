b9a276f9868e76bb189bcec4cce197ce25e3964d
tufei <tufei@maka.im>
2016-06-13 18:27:06 +0800

    1,delete the test file (index.html) 2,add the updateTag.sh 3,updateTag is remove the tag args named, and create the new tag, push to remote

---
+++ updateTag.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# echo $# < 0
+if [[ $# < 1 ]]; then
+    echo "Usage: UpdateJS.sh <tagname>"
+    exit 1
+fi
+
+# 拿到当前仓库名
+repository=`git remote get-url origin`
+
+echo "local repository is $repository"
+
+# 如果远程有这个分支，则删掉
+if [[ `git ls-remote --tags $repository | grep -o refs/tags/$1` ]]; then
+    echo "remote have tag $1"
+    echo "delete remote tag $1..."
+    git push --delete origin tag $1
+else
+    echo "remote don't have tag $1"
+fi
+
+
+if [[ `git tag | grep -o $1` ]]; then
+    #statements
+    echo "local have tag $1"
+    echo "delete local tag $1..."
+    git tag -d $1
+else
+    echo "local don't have tag $1"
+fi
+
+echo "sync code..."
+git fetch
+
+# 在当前本地分支创建分支
+echo "from master create local tag $1..."
+git tag $1
+echo "create local tag $1 done"
+
+# 分支推送到远程
+echo "push local tag $1 to remote"
+git push origin $1
+echo "push local tag $1 done"
+
+echo "Done"
